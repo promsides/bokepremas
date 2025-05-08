6a444400100632d9af8d8d1ba4c15b74d7679c66
promsides <161449988+promsides@users.noreply.github.com>
2025-05-09 03:46:55 +0700

    Update navbar.tsx

--- components/layouts/navbar.tsx
+++ components/layouts/navbar.tsx
@@ -145,16 +145,7 @@ const Navbar = async () => {
                 </Link>
             </div>
             <div className="flex flex-row space-x-2">
-                <div className="hidden md:flex items-center gap-4">
-			<Link
-                        href="https://ukhtismp.pages.dev"
-                        className="hover:bg-accent px-2 py-1.5 rounded-md"
-                        aria-label="Bokep Satset"
-                    	>
-                        Bokep Smp
-                    	</Link>
-                    <NavMenu folders={folders}></NavMenu>
-                </div>
+                
                 <SearchInput className="hidden md:block w-full md:w-[260px] lg:w-[300px]" />
                 <SearchDialog></SearchDialog>
             </div>
